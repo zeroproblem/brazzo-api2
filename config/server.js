d828548298c3987981b5905b26900755fe2e2573
Zero Problem <37811022+zeroproblem@users.noreply.github.com>
2025-10-23 19:57:00 -0300

    Add PUBLIC_URL and proxy configuration to server

--- config/server.js
+++ config/server.js
@@ -2,6 +2,8 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   //host: env('HOST', 'https://apibrazzo.zeroproblem.com.br'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', undefined),
+  proxy: { koa: true },
   app: {
     keys: env.array('APP_KEYS'),
   },
