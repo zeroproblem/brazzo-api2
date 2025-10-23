8377fdfc3e078b6c8058829b5e9cecfb9119562e
Zero Problem <37811022+zeroproblem@users.noreply.github.com>
2025-10-23 18:21:57 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,11 +4,16 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'brazzo-connection_postgres'),
+      //host: env('DATABASE_HOST', 'brazzo-connection_postgres'),
+      host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'brazzodb'),
-      user: env('DATABASE_USERNAME', 'brazzoadmin'),
-      password: env('DATABASE_PASSWORD', 'Br4zzoDB#'),
+      //database: env('DATABASE_NAME', 'brazzodb'),
+      //user: env('DATABASE_USERNAME', 'brazzoadmin'),
+      //password: env('DATABASE_PASSWORD', 'Br4zzoDB#'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      
       ssl: env.bool('DATABASE_SSL', false) && {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
       },
