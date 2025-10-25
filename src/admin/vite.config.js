3d2821e9b43f2c03f1aeb8e6653f0eae19778a00
Zero Problem <37811022+zeroproblem@users.noreply.github.com>
2025-10-24 21:09:38 -0300

    Add allowed host for API domain in Vite config

--- src/admin/vite.config.js
+++ src/admin/vite.config.js
@@ -12,6 +12,7 @@ module.exports = (config) => {
           allowedHosts: [
            'localhost',
            '127.0.0.1',
+           'apibrazzo.zeroproblem.com.br'  // 👈 add your domain here
            'brazzodev.zeroproblem.com.br'  // 👈 add your domain here
           ],
     port: 1337, // optional, only if you run the admin separately
